0f1bea8ad8537ad0db2156b86d655801f0b3c3cd
matt-pasek <mpasek@tm1.edu.pl>
2022-10-14 11:48:35 +0200

    style and store config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,23 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as baseUseStore } from "vuex";
 
-export default createStore({
-  state: {},
+export interface State {
+  count: number;
+}
+
+// define injection key
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore<State>({
+  state: {
+    count: 0,
+  },
   getters: {},
   mutations: {},
   actions: {},
   modules: {},
 });
+
+export function useStore() {
+  return baseUseStore(key);
+}
