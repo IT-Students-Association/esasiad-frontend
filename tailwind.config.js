91dd8f7acf439a6802f5de10bc01097498306e6e
matt-pasek <mpasek@tm1.edu.pl>
2022-10-14 11:37:43 +0200

    tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#436146",
+        main2: "#4F7F5F",
+        accent1: "#B2DCA2",
+        accent2: "#FDDF92",
+        white: "#F7F7F7",
+      },
+      fontFamily: {
+        overpass: ["Overpass", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
