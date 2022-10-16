c3adc3c260406e481f58a0cea3a27b5055c93cff
matt-pasek <mpasek@tm1.edu.pl>
2022-10-16 17:50:38 +0200

    finished homepage v1

--- src/components/HomeNavbar.vue
+++ src/components/HomeNavbar.vue
@@ -2,18 +2,18 @@
   <nav
     class="fixed w-[90vw] mx-auto pt-3 flex justify-between items-center z-50 navbar-blur left-0 right-0"
   >
-    <h2 class="font-overpass text-main text-xl font-semibold">e-Sąsiad</h2>
-    <div class="flex w-1/5 justify-between invisible sm:visible">
+    <h2 class="text-main text-xl font-semibold">e-Sąsiad</h2>
+    <div class="flex sm:w-1/5 justify-between">
       <router-link to="/login">
         <p
-          class="font-overpass text-lg font-normal px-6 py-3 rounded-3xl bg-orange"
+          class="text-s sm:text-lg font-normal px-7 sm:pt-3 py-2 rounded-3xl bg-none sm:bg-orange"
         >
           dołącz
         </p>
       </router-link>
       <router-link to="/register">
         <p
-          class="font-overpass text-lg font-normal px-6 py-3 rounded-3xl bg-orange"
+          class="text-s sm:text-lg font-normal px-7 sm:pt-3 py-2 rounded-3xl bg-none sm:bg-orange"
         >
           zaloguj
         </p>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
   <HomeNavbar />
   <div class="h-screen relative w-screen background-gradient overflow-x-hidden">
-    <div class="container-grid pt-60">
+    <div class="container-grid pt-28 sm:pt-60">
       <div class="row z-20">
         <div class="col-1" />
         <div class="col-4">
           <h1
-            class="text-[#000000] opacity-50 text-center w-screen w-full sm:text-left"
+            class="transparent-black text-center w-screen w-full sm:text-left"
           >
             Zbuduj z nami nowoczesne sąsiedztwa.
           </h1>
@@ -14,9 +14,9 @@
       </div>
       <div class="row z-20">
         <div class="col-1" />
-        <div class="col-4 text-main2">
-          <h2 class="">Lorem ipsum sit dolor amit.</h2>
-          <h2 class="">
+        <div class="col-4 transparent-black">
+          <h2>Lorem ipsum sit dolor amit.</h2>
+          <h2>
             Dołącz do nas i zmieniaj z nami obraz osiedla 21 wieku. Lorem ipsum
             dolor sit amet, consectetur...
           </h2>
@@ -35,22 +35,149 @@
       </div>
       <div class="row z-20 mt-16">
         <div class="col-1" />
-        <div class="col-10 flex">
+        <div
+          class="col-10 flex flex-col sm:flex-row justify-between items-center"
+        >
           <div>
-            <div class="flex items-center">
+            <div class="flex items-center justify-center sm:justify-start mb-3">
               <img
-                class="w-20 h-20"
-                src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                class="w-16"
+                src="@/assets/icons/help.png"
+                alt="people helping each other icon"
               />
-              <h3 class="ml-4 text-main2">Lorem ipsum</h3>
+              <h3 class="ml-4 mt-2 text-main2">Użytkowo xd</h3>
             </div>
-            <h2 class="">Kurwa</h2>
-            <p>Lorem ipsum</p>
+            <h2 class="text-main2">Nowa definicja pomocy sąsiedzkiej.</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur
+              vel bibendum lorem. Morbi convallis convallis diam sit amet
+              lacinia. Aliquam in elementum tellus.
+            </p>
           </div>
-          <img class="w-1/2 ml-16" src="@/assets/mockups/mockup1.png" />
+          <img
+            class="w-10/12 sm:w-5/12 ml-16"
+            src="@/assets/mockups/mockup1.png"
+            alt="app mockup"
+          />
         </div>
       </div>
-      <div class="row z-20"></div>
+      <div class="row z-20 mt-40 sm:mt-96">
+        <div class="col-1" />
+        <div
+          class="col-10 flex flex-col sm:flex-row justify-between items-center"
+        >
+          <img
+            class="w-full sm:w-5/12 mr-0 sm:mr-16"
+            src="@/assets/images/neighbors.png"
+            alt="app mockup"
+          />
+          <div>
+            <div class="flex items-center justify-center sm:justify-end mb-3">
+              <h3 class="mr-4 mt-2 transparent-black">Społecznie</h3>
+              <img
+                class="w-16"
+                src="@/assets/icons/forum.png"
+                alt="forum of people icon"
+              />
+            </div>
+            <h2 class="transparent-black text-center sm:text-right">
+              Poznaj swoich sąsiadów.
+            </h2>
+            <p class="text-center sm:text-right">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur
+              vel bibendum lorem. Morbi convallis convallis diam sit amet
+              lacinia. Aliquam in elementum tellus.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row z-20 mt-64">
+        <div class="col-1" />
+        <div
+          class="col-10 flex flex-col sm:flex-row justify-between items-center"
+        >
+          <div>
+            <div class="flex items-center justify-center sm:justify-start mb-3">
+              <img
+                class="w-16"
+                src="@/assets/icons/help.png"
+                alt="people helping each other icon"
+              />
+              <h3 class="ml-4 mt-2 text-main2">Użytkowo xd</h3>
+            </div>
+            <h2 class="text-main2">
+              Rywalizuj ze swoimi sąsiadami.<br />
+              Zdrowo i korzystnie
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra, per inceptos himenaeos. Praesent auctor purus
+              luctus enim egestas, ac scelerisque ante pulvinar. Donec ut
+              rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur
+              vel bibendum lorem. Morbi convallis convallis diam sit amet
+              lacinia. Aliquam in elementum tellus.
+            </p>
+          </div>
+          <img
+            class="w-8/12 sm:w-5/12 ml-16"
+            src="@/assets/mockups/mockup2.png"
+            alt="app mockup"
+          />
+        </div>
+      </div>
+      <div class="row z-20 mt-16">
+        <div class="col-2" />
+        <div class="col-8">
+          <h1 class="transparent-black text-center">
+            Twórcy. Z doświadczeniem.
+          </h1>
+        </div>
+      </div>
+      <div class="row z-20">
+        <div class="col-3" />
+        <div class="col-6 flex justify-center">
+          <img
+            class="w-9/12"
+            src="@/assets/images/creators.png"
+            alt="creators of the app"
+          />
+        </div>
+      </div>
+      <div class="row z-20">
+        <div class="col-2" />
+        <div class="col-8 flex justify-center">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+            feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus
+            enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
+            Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum
+            lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in
+            elementum tellus.
+          </p>
+        </div>
+      </div>
     </div>
     <div class="home-image-div z-10 polygon bg-[#F7F7F7]">
       <img src="@/assets/images/ludki_full.png" alt="" class="home-image" />
@@ -58,6 +185,9 @@
     <div class="home-image-div z-0">
       <img src="@/assets/images/ludki_line.png" alt="" class="home-image" />
     </div>
+    <div
+      class="polygon2 bg-[#F7F7F7] absolute top-[290%] sm:top-[220%] left-0 right-0 w-screen h-[250vh] sm:h-[200vh]"
+    ></div>
   </div>
 </template>
 
@@ -69,6 +199,9 @@ export default defineComponent({
   name: "HomeView",
   components: {
     HomeNavbar,
+  },
+  mounted() {
+    document.title = "e-Sąsiad | Strona Główna";
   },
 });
 </script>
