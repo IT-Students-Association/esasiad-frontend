6028b97cc0db9a73abd8f3251b8387c0d1dfe19d
matt-pasek <mpasek@tm1.edu.pl>
2022-10-17 13:36:49 +0200

    store basic config for authorization + pwa checker

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,19 +2,46 @@ import { InjectionKey } from "vue";
 import { createStore, Store, useStore as baseUseStore } from "vuex";
 
 export interface State {
-  count: number;
+  auth: boolean;
+  admin: boolean;
+  isPwa: boolean;
 }
+
+const isPwa = window.matchMedia("(display-mode: standalone)").matches;
 
 // define injection key
 export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
   state: {
-    count: 0,
+    auth: false,
+    admin: false,
+    isPwa: isPwa,
   },
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    isAuthenticated: (state) => {
+      return state.auth;
+    },
+  },
+  mutations: {
+    setAuth: (state, auth) => {
+      state.auth = auth;
+    },
+  },
+  actions: {
+    login({ commit }, formData) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (formData.email == "test@test.pl" && formData.password == "admin") {
+            commit("setAuth", true);
+            resolve(true);
+          } else {
+            reject();
+          }
+        }, 1000);
+      });
+    },
+  },
   modules: {},
 });
 
