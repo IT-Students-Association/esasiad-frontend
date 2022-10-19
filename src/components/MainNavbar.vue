ce28d507825f8f59f18562029a6fd4eb427e2422
matt-pasek <mpasek@tm1.edu.pl>
2022-10-19 17:59:43 +0200

    Quick MainNavbar fixes

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,20 +1,22 @@
 <template>
   <nav
-    class="fixed w-full mx-auto pt-10 pb-10 flex items-center bg-[#F7F7F7] z-50 left-0 right-0"
+    class="fixed w-screen py-5 flex justify-center bg-[#F7F7F7] z-50 left-0 right-0"
   >
-    <h2 class="text-main text-x ml-20 font-semibold">e-Sąsiad</h2>
-    <div class="flex mx-auto sm:w-1/3 justify-evenly">
-      <p>Społeczność</p>
-      <router-link to="/help">
-        <p>Pomoc</p>
-      </router-link>
-      <p>Zgłoś usterkę</p>
-      <p>Ranking</p>
-    </div>
-    <div class="flex sm:w-2/12">
-      <img src="@/assets/icons/notifs.png" class="nav-icons" />
-      <img src="@/assets/icons/helper.png" class="nav-icons" />
-      <img src="@/assets/icons/settings.png" class="nav-icons px-5" />
+    <div class="flex w-[90%] justify-between items-center">
+      <h2 class="text-main text-xl font-semibold">e-Sąsiad</h2>
+      <div class="flex sm:w-1/3 justify-evenly">
+        <p>Społeczność</p>
+        <router-link to="/help">
+          <p>Pomoc</p>
+        </router-link>
+        <p>Zgłoś usterkę</p>
+        <p>Ranking</p>
+      </div>
+      <div class="flex sm:w-2/12">
+        <img src="@/assets/icons/notifs.png" class="nav-icons" />
+        <img src="@/assets/icons/helper.png" class="nav-icons" />
+        <img src="@/assets/icons/settings.png" class="nav-icons" />
+      </div>
     </div>
   </nav>
 </template>
