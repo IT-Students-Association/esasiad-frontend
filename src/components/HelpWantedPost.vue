dc1879a69097eee68917cdac7f8f0feba45ea5a0
matt-pasek <mpasek@tm1.edu.pl>
2022-10-20 16:34:54 +0200

    basic layout of help subpage
    Signed-off-by: matt-pasek <mpasek@tm1.edu.pl>

---
+++ src/components/HelpWantedPost.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex bg-white !py-5 px-5 rounded-3xl flex-col w-full sm:max-w-[33%] box"
+  >
+    <p class="font-light">e-Sąsiad HQ, 1 godzinę temu</p>
+    <div class="flex items-center gap-1">
+      <img src="@/assets/icons/user.svg" class="w-12 h-auto" alt="user" />
+      <div>
+        <p>Mateusz Pasek</p>
+        <p class="font-light">Potężny Sąsiad | 9999pkt</p>
+      </div>
+    </div>
+    <h2 class="text-lg font-semibold">
+      Testowa prośba o pomoc od Administracji serwisu e-Sąsiad
+    </h2>
+    <button
+      class="flex lg:w-7/12 2xl:w--1/2 justify-evenly items-center bg-main2 text-white text-center rounded-lg px-4 py-2 mt-2"
+    >
+      <img src="@/assets/icons/like.svg" class="w-6 h-auto" alt="like" />
+      Zgłoś chęć pomocy
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelpWantedPost",
+});
+</script>
--- src/views/mainframe/HelpView.vue
+++ src/views/mainframe/HelpView.vue
@@ -1,11 +1,54 @@
 <template>
-  <h1>Haha</h1>
+  <div class="my-10 flex flex-col mx-10">
+    <div class="flex gap-5">
+      <label>
+        <input
+          type="radio"
+          id="recent"
+          name="filter"
+          value="recent"
+          class="peer"
+          checked
+          hidden
+        />
+        <div class="labeled">Najnowsze prośby</div>
+      </label>
+      <label>
+        <input
+          type="radio"
+          id="nearby"
+          name="filter"
+          value="nearby"
+          class="peer"
+          hidden
+        />
+        <div class="labeled">Prośby z okolicy</div>
+      </label>
+    </div>
+    <div class="flex justify-around flex-wrap gap-10">
+      <HelpPost />
+      <HelpPost />
+      <HelpPost />
+      <HelpPost />
+      <HelpPost />
+      <HelpPost />
+      <HelpPost />
+      <HelpPost />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HelpPost from "@/components/HelpWantedPost.vue";
 
 export default defineComponent({
   name: "HelpView",
+  mounted() {
+    document.title = "e-Sąsiad | Pomoc";
+  },
+  components: { HelpPost },
 });
 </script>
+
+<style lang="scss" scoped></style>
