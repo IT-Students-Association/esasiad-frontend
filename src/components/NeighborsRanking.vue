82549e89720379279038a887d635231962860db0
matt-pasek <mpasek@tm1.edu.pl>
2022-10-22 18:28:57 +0200

    static ranking
    Signed-off-by: matt-pasek <mpasek@tm1.edu.pl>

---
+++ src/components/NeighborsRanking.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flex flex-col gap-10 mt-5">
+    <div class="flex translate-x-28">
+      <div
+        class="flex flex-col items-center justify-center box !p-2 w-1/3 z-50"
+      >
+        <h2 class="font-medium text-lg">1.</h2>
+        <h2 class="font-medium text-base text-center">Anna Stefanek</h2>
+        <div class="flex justify-evenly w-2/3">
+          <p class="text-base text-main2">21344</p>
+          <p class="text-base text-black text-opacity-50">200</p>
+        </div>
+      </div>
+      <div
+        class="flex flex-col items-center justify-center box !p-2 w-1/3 z-40 -translate-x-5 translate-y-20"
+      >
+        <h2 class="font-medium text-lg">2.</h2>
+        <h2 class="font-medium text-base text-center">Renata Kapusta</h2>
+        <div class="flex justify-evenly w-2/3">
+          <p class="text-base text-main2">17213</p>
+          <p class="text-base text-black text-opacity-50">179</p>
+        </div>
+      </div>
+      <div
+        class="flex flex-col items-center justify-center box !p-2 w-1/3 z-30 -translate-x-96 translate-y-40"
+      >
+        <h2 class="font-medium text-lg">3.</h2>
+        <h2 class="font-medium text-base text-center">Barbara Przekorzystna</h2>
+        <div class="flex justify-evenly w-2/3">
+          <p class="text-base text-main2">12021</p>
+          <p class="text-base text-black text-opacity-50">100</p>
+        </div>
+      </div>
+    </div>
+    <table
+      class="mt-40 table-auto gap-5 border-separate border-spacing-2 box !p-3"
+    >
+      <thead>
+        <tr>
+          <th class="font-medium">Miejsce</th>
+          <th class="font-medium">Imię i nazwisko</th>
+          <th class="text-main2">Punkty</th>
+          <th class="text-black text-opacity-50">Pomagani sąsiedzi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>4.</td>
+          <td>Anna Potopowa</td>
+          <td class="text-main2">400</td>
+          <td class="text-black text-opacity-50">100</td>
+        </tr>
+        <tr>
+          <td>5.</td>
+          <td>Anna Achmistrowicz-Wachmistrowicz</td>
+          <td class="text-main2">400</td>
+          <td class="text-black text-opacity-50">100</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NeighborsRanking",
+});
+</script>
+
+<style lang="scss" scoped>
+td {
+  @apply text-center w-1/4;
+}
+</style>
--- src/views/mainframe/RankingView.vue
+++ src/views/mainframe/RankingView.vue
@@ -1,11 +1,91 @@
 <template>
-  <h1>Haha</h1>
+  <div class="flex flex-col mx-auto w-11/12 my-10">
+    <h2 class="text-lg text-main font-semibold">Ranking Sąsiadów</h2>
+    <div class="flex gap-5 mt-7">
+      <label>
+        <input
+          type="radio"
+          id="n1"
+          name="filter"
+          value="1"
+          class="peer"
+          checked
+          hidden
+        />
+        <div class="labeled2">Ogólny</div>
+      </label>
+      <label>
+        <input
+          type="radio"
+          id="n1"
+          name="filter"
+          value="1"
+          class="peer"
+          hidden
+        />
+        <div class="labeled2">na Wspólnej 21/37</div>
+      </label>
+      <label>
+        <input
+          type="radio"
+          id="n2"
+          name="filter"
+          value="2"
+          class="peer"
+          hidden
+        />
+        <div class="labeled2">na Wiśniowej 56</div>
+      </label>
+      <label>
+        <input
+          type="radio"
+          id="n2"
+          name="filter"
+          value="2"
+          class="peer"
+          hidden
+        />
+        <div class="labeled2">“Pod mosteczkiem”</div>
+      </label>
+    </div>
+    <div class="flex justify-between gap-5">
+      <NeighborsRanking />
+      <div class="flex flex-col gap-5 w-1/3">
+        <div class="box px-5 py-4">
+          <h2 class="text-lg">Dziękujemy, że jesteś</h2>
+          <p class="text-sm">Sąsiedzi którzy otrzymali od Ciebie pomoc:</p>
+          <ul class="text-sm">
+            <li>W tym tygodniu: 3</li>
+            <li>Ogólnie: 100</li>
+          </ul>
+        </div>
+        <div class="box px-5 py-4">
+          <h2 class="text-lg">Mkniesz do przodu</h2>
+          <p class="text-sm">
+            Grażyna, brakuje Ci 6969 punktów do uzyskania tytułu
+            <span class="text-main2">Sąsiad z doświadczeniem</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import NeighborsRanking from "@/components/NeighborsRanking.vue";
 
 export default defineComponent({
   name: "RankingView",
+  components: { NeighborsRanking },
+  mounted() {
+    document.title = "e-Sąsiad | Ranking";
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+.labeled2 {
+  @apply text-main2 transition-all translate-y-0 peer-checked:-translate-y-7 peer-checked:text-base;
+}
+</style>
