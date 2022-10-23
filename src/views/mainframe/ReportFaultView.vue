a5ede03adb10fabf1848d96b9787690da1b03703
matt-pasek <mpasek@tm1.edu.pl>
2022-10-23 17:25:20 +0200

    report view - mobile modal for reporting form
    Signed-off-by: matt-pasek <mpasek@tm1.edu.pl>

--- src/views/mainframe/ReportFaultView.vue
+++ src/views/mainframe/ReportFaultView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col mx-auto my-10 portrait:mt-24 portrait:mx-5 portrait:items-center"
+    class="flex flex-col mx-5 my-10 portrait:mt-24 portrait:mx-5 portrait:items-center"
   >
     <h2 class="text-lg text-main font-semibold portrait:hidden">
       Zgłoś usterkę
@@ -13,6 +13,7 @@
         src="@/assets/icons/add.svg"
         alt="add icon"
         class="h-12 bg-accent2 rounded-2xl"
+        @click="showModalReport"
       />
     </div>
     <div class="mt-3 bg-white box !py-5 px-5 rounded-3xl portrait:hidden">
@@ -24,7 +25,7 @@
           </div>
           <div class="flex flex-col">
             <label class="title" for="categories">Kategorie</label>
-            <div class="flex gap-2">
+            <div class="flex gap-2 flex-wrap transition-all">
               <label>
                 <input
                   type="radio"
@@ -79,9 +80,16 @@
             <label class="title" for="description">Opis</label>
             <textarea id="description" v-model="description" />
           </div>
+          <button
+            class="bg-accent2 w-1/4 text-black font-semibold pt-3 pb-2 px-4 rounded-xl"
+          >
+            Zgłoś
+          </button>
         </form>
         <div>
-          <p>Tu będzie upo mapa</p>
+          <p>
+            Tu będzie mapa, na której będzie wybierało się gdzie jest problem
+          </p>
         </div>
       </div>
     </div>
@@ -97,12 +105,105 @@
         <ReportPost :post="post" />
       </div>
     </div>
+    <Modal v-model="isShowReport" :close="closeModalReport">
+      <div
+        class="bg-opacity-70 bg-[#000000] w-screen h-screen flex justify-center items-center"
+      >
+        <div class="mt-3 bg-white box !py-5 px-5 mx-5 rounded-3xl">
+          <button @click="closeModalReport">
+            <img
+              class="w-8"
+              src="@/assets/icons/cross-1.svg"
+              alt="close icon"
+            />
+          </button>
+          <form
+            class="flex flex-col gap-5 w-full mt-5"
+            @submit.prevent="reportFault"
+          >
+            <div>
+              <p>
+                Tu będzie mapa, na której będzie wybierało się gdzie jest
+                problem
+              </p>
+            </div>
+            <div class="flex flex-col">
+              <label class="title" for="title">Tytuł</label>
+              <input id="title" type="text" v-model="title" />
+            </div>
+            <div class="flex flex-col">
+              <label class="title" for="categories">Kategorie</label>
+              <div class="flex gap-2">
+                <label>
+                  <input
+                    type="radio"
+                    id="vandalism"
+                    name="categories"
+                    value="vandalism"
+                    class="peer"
+                    checked
+                    hidden
+                  />
+                  <div class="category-label !text-s">Wandalizm</div>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    id="lighting"
+                    name="categories"
+                    value="lighting"
+                    class="peer"
+                    checked
+                    hidden
+                  />
+                  <div class="category-label !text-s">Oświetlenie</div>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    id="trash"
+                    name="categories"
+                    value="trash"
+                    class="peer"
+                    checked
+                    hidden
+                  />
+                  <div class="category-label !text-s">Odpady</div>
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    id="other"
+                    name="categories"
+                    value="other"
+                    class="peer"
+                    checked
+                    hidden
+                  />
+                  <div class="category-label !text-s">Inne</div>
+                </label>
+              </div>
+            </div>
+            <div class="flex flex-col">
+              <label class="title" for="description">Opis</label>
+              <textarea id="description" v-model="description" />
+            </div>
+            <div class="flex justify-end">
+              <button
+                class="bg-accent2 text-black font-semibold pt-3 pb-2 px-4 rounded-xl"
+              >
+                Zgłoś
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Modal>
   </div>
-  <Modal v-model="isShowReport" :close="closeModalReport"> </Modal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import ReportPost from "@/components/ReportPost.vue";
 import { useStore } from "@/store";
 
@@ -115,7 +216,24 @@ export default defineComponent({
       posts: store.state.reportPosts,
       description: "",
       title: "",
-      isShowReport: false,
+    };
+  },
+  setup() {
+    const inputs = document.querySelectorAll("input");
+    const isShowReport = ref(false);
+    function showModalReport() {
+      isShowReport.value = true;
+    }
+    function closeModalReport() {
+      isShowReport.value = false;
+      inputs.forEach((input) => {
+        input.value = "";
+      });
+    }
+    return {
+      isShowReport,
+      showModalReport,
+      closeModalReport,
     };
   },
   mounted() {
